a12726da8b7f7bb772855809972f2ffff535dc00
mauroociappinaph <ciappinamauroo@gmail.com>
2023-04-08 19:38:18 -0300

    Modificación  de getCharById

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -3,20 +3,21 @@ const axios = require("axios");
 const URL = "http://be-a-rym.up.railway.app/api"
 const KEY = "09d38d0a379a.47ee58661aa6af2a95d7";
 
+const success = (response, res) => {
+    // Si la petición es exitosa, hacemos algo con los datos recibidos
+    const {id, image, name, gender ,species } = response.data;
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end (JSON.stringify ({id, image, name, gender ,species}))
+}
 
-function getCharById(res, id) {
+const getCharById = (res, id) => {
 
 
 
   // Hacemos la petición utilizando Axios
   axios
     .get(`${URL}/character/${id}`)
-    .then((response) => {
-      // Si la petición es exitosa, hacemos algo con los datos recibidos
-      const {id, image, name, gender ,species } = response.data;
-      res.writeHead(200, {"Content-Type": "application/json"});
-      res.end (JSON.stringify ({id, image, name, gender ,species}))
-    })
+    .then(response => success(response,res))
     
     .catch(error => {
       // Si hay algún error, lo manejamos aquí
